e33ec52fe8d276637093a3bc07ef516d5fcd1c38
liqd <liqd@efly.cc>
2022-02-24 17:43:22 +0800

    electrol

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 500,
+        height: 750
+    })
+
+    win.loadFile('./public/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
+})
